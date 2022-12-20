2656dd43fd4b4a1c6153a205e48103d50f88f13a
sakertooth <sakertooth@gmail.com>
2022-12-19 22:46:22 -0500

    Implement StretchOneSemitone test

--- tests/StretchOneSemitone.cpp
+++ tests/StretchOneSemitone.cpp
@@ -1,8 +1,14 @@
 #include "RubberbandHelper.hpp"
 #include "AudioBuffer.hpp"
+#include <cmath>
 
 int main() 
 {
     auto audioBuffer = shone::core::AudioBuffer{"files/Sine_wave_440.wav"};
-    
+    auto rubberBandStretcher = RubberBand::RubberBandStretcher{static_cast<size_t>(audioBuffer.sampleRate()), 2};
+    rubberBandStretcher.setPitchScale(std::pow(2.0, 1 / 12.0));
+
+    auto pithedBuffer = shone::core::RubberbandHelper::executeRubberband(rubberBandStretcher, audioBuffer.audioFrames());
+    auto pitchedFileBuffer = shone::core::AudioBuffer{pithedBuffer, audioBuffer.sampleRate()};
+    pitchedFileBuffer.writeToDisk("files/Sine_wave_440_1ct.wav", SF_FORMAT_WAV | SF_FORMAT_PCM_24);
 }
