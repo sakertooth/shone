dc46f2bae6294cd1bac828050b5b1b346083f32a
sakertooth <sakertooth@gmail.com>
2022-12-19 23:20:53 -0500

    Make adjustments to AudioBuffer

--- src/core/AudioBuffer.cpp
+++ src/core/AudioBuffer.cpp
@@ -4,78 +4,85 @@
 #include <cmath>
 
 #include "AudioBuffer.hpp"
+#include "Defaults.hpp"
 #include "Mix.hpp"
 
 namespace shone::core
 {
-    AudioBuffer::AudioBuffer(const std::size_t size) : m_audioFrames(size) {}
+    AudioBuffer::AudioBuffer(const std::size_t size) : 
+        m_audioFrames(size),
+        m_originalSampleRate(DEFAULT_SAMPLE_RATE),
+        m_originalNumChannels(DEFAULT_NUM_CHANNELS) {}
 
     AudioBuffer::AudioBuffer(const std::filesystem::path& filePath) : m_filePath(filePath)
     {
-        auto [audioFile, audioInfo] = openAudioHandle(filePath, SFM_READ);
+        auto audioInfo = SF_INFO{};
+        auto audioFile = openAudioHandle(filePath, audioInfo, SFM_READ);
         auto sampleData = std::vector<float>(audioInfo.frames * audioInfo.channels);
         auto samplesRead = sf_read_float(audioFile, sampleData.data(), sampleData.size());
 
-        if (samplesRead != sampleData.size()) 
+        if (samplesRead != static_cast<int>(sampleData.size())) 
         {
             throw std::runtime_error{"Could not read all required samples in audio file"};
         }
         
-        if (audioInfo.channels < 2) 
+        if (audioInfo.channels < DEFAULT_NUM_CHANNELS) 
         {
             m_audioFrames = Mix::mixMonoToStereo(sampleData);
         }
-        else if (audioInfo.channels > 2)
+        else if (audioInfo.channels > DEFAULT_NUM_CHANNELS)
         {
             m_audioFrames = Mix::mixDownToStereo(sampleData, audioInfo.channels);
         }
         else 
         {
             m_audioFrames = std::vector<AudioFrame>(audioInfo.frames);
-            std::copy(sampleData.begin(), sampleData.end(), m_audioFrames.begin()->data());
+            for (int i = 0; i < static_cast<int>(sampleData.size()); i += DEFAULT_NUM_CHANNELS)
+            {
+                m_audioFrames.push_back(AudioFrame{sampleData[i], sampleData[i + 1]});
+            }
         }
 
-        m_sampleRate = audioInfo.samplerate;
+        m_originalNumChannels = audioInfo.channels;
+        m_originalSampleRate = audioInfo.samplerate;
         sf_close(audioFile);
     }
 
-    void AudioBuffer::writeToDisk(const std::filesystem::path& outputFilePath) 
+    AudioBuffer::AudioBuffer(const std::vector<AudioFrame>& audioFrames) : 
+        m_audioFrames(audioFrames),
+        m_originalNumChannels(DEFAULT_NUM_CHANNELS)
+    {}
+
+    void AudioBuffer::writeToDisk(const std::filesystem::path& path, int format) 
     {
-        auto [audioFile, audioInfo] = openAudioHandle(outputFilePath, SFM_WRITE);
+        auto audioInfo = SF_INFO{};
+        audioInfo.format = format;
+        audioInfo.channels = DEFAULT_NUM_CHANNELS;
+        audioInfo.samplerate = DEFAULT_SAMPLE_RATE;
+        
+        auto audioFile = openAudioHandle(path, audioInfo, SFM_WRITE);
         sf_writef_float(audioFile, m_audioFrames.data()->data(),  m_audioFrames.size());
-        sf_write_sync(audioFile);
         sf_close(audioFile);
     }
 
-    const std::filesystem::path AudioBuffer::filePath() const 
+    const std::optional<std::filesystem::path> AudioBuffer::filePath() const 
     {
         return m_filePath;
     }
-
-    const std::vector<AudioFrame>& AudioBuffer::audioFrames() const 
-    {
-        return m_audioFrames;
-    }
     
-    int AudioBuffer::sampleRate() const 
-    {
-        return m_sampleRate;
-    }
-
-    std::pair<SNDFILE*, SF_INFO> AudioBuffer::openAudioHandle(const std::filesystem::path& filePath, int mode) 
+    SNDFILE* AudioBuffer::openAudioHandle(const std::filesystem::path& filePath, SF_INFO& info, int mode) 
     {
         auto nativeFilePath = filePath.c_str();
         SNDFILE* audioFile = nullptr;
-        auto audioInfo = SF_INFO{};
-
+        
         if (std::is_same_v<std::filesystem::path::value_type, char>) 
         {
-            audioFile = sf_open(nativeFilePath, mode, &audioInfo);
+            audioFile = sf_open(nativeFilePath, mode, &info);
         }
         #ifdef WIN32
         else if (std::is_same_v<std::filesystem::path::value_type, wchar_t>) 
         {
-            audioFile = sf_wchar_open(nativeFilePath, mode, &audioInfo);
+            audioFile = sf_wchar_open(nativeFilePath, mode, &info);
         }
         #endif
         else 
@@ -83,11 +90,26 @@ namespace shone::core
             throw std::domain_error{"Unknown/unsupported file path.\n"};
         }
 
-        if (sf_error(audioFile)) 
+        if (sf_error(audioFile))
         {
             throw std::runtime_error{std::string{"SndFile error in AudioBuffer::openAudioBuffer: "} + sf_strerror(audioFile)};
         }
 
-        return {audioFile, audioInfo};
+        return audioFile;
+    }
+
+    const std::vector<AudioFrame>& AudioBuffer::audioFrames() const 
+    {
+        return m_audioFrames;
+    }
+    
+    int AudioBuffer::originalSampleRate() const 
+    {
+        return m_originalSampleRate;
+    }
+
+    int AudioBuffer::originalNumChannels() const 
+    {
+        return m_originalNumChannels;
     }
 }
--- src/core/AudioBuffer.hpp
+++ src/core/AudioBuffer.hpp
@@ -2,6 +2,7 @@
 #include "AudioFrame.hpp"
 #include <vector>
 #include <filesystem>
+#include <optional>
 #include <sndfile.h>
 
 namespace shone::core
@@ -10,20 +11,22 @@ namespace shone::core
     {
     public:
         AudioBuffer(const std::filesystem::path& filePath);
-        AudioBuffer(const std::size_t size);
+        AudioBuffer(const std::vector<AudioFrame>& frames);
+        explicit AudioBuffer(std::size_t size);
 
-        void writeToDisk(const std::filesystem::path& outputFilePath);
+        void writeToDisk(const std::filesystem::path& path, int format);
 
-        const std::filesystem::path filePath() const;
+        const std::optional<std::filesystem::path> filePath() const;
         const std::vector<AudioFrame>& audioFrames() const;
-        int sampleRate() const;
+        int originalSampleRate() const;
+        int originalNumChannels() const;
+    private:
+        SNDFILE* openAudioHandle(const std::filesystem::path& filePath, SF_INFO& info, int mode);
 
     private:
-        std::pair<SNDFILE*, SF_INFO> openAudioHandle(const std::filesystem::path& filePath, int mode);
-
-    private:
-        std::filesystem::path m_filePath;
+        std::optional<std::filesystem::path> m_filePath;
         std::vector<AudioFrame> m_audioFrames;
-        int m_sampleRate = 0;
+        int m_originalSampleRate = 0;
+        int m_originalNumChannels = 0;
     };
 }
