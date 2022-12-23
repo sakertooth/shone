94e537a04bff1a77fdbfb98948abf45e4ecf6ad0
sakertooth <sakertooth@gmail.com>
2022-12-23 03:26:58 -0500

    Simplify ReadAudioFile test

--- tests/ReadAudioFile.cpp
+++ tests/ReadAudioFile.cpp
@@ -5,13 +5,8 @@
 
 int main()
 {
-    // sample rate = 132000, duration = 5s, 1 channel
+    // original sample rate = 132000, duration = 5s (660000 samples), 1 channel
     const auto filePath = "files/Sine_wave_440.wav";
     const auto audioBuffer = shone::core::AudioBuffer{filePath};
-    
-    assert(audioBuffer.filePath() == filePath);
-    assert(audioBuffer.sampleRate() == 132000);
-    assert(audioBuffer.originalNumChannels() == 1);
-    assert(audioBuffer.audioFrames().size() == 660000 * 2);
-    assert(static_cast<int>(audioBuffer.audioFrames().size() / 2 / static_cast<double>(audioBuffer.sampleRate())) == 5);
+    audioBuffer.writeToDisk("files/Sine_wave_440_read_verify.wav", SF_FORMAT_WAV | SF_FORMAT_PCM_24);
 }
