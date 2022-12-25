dfdab44fa779a5d434c06b8356d1808050997974
sakertooth <sakertooth@gmail.com>
2022-12-25 15:37:59 -0500

    Add ReadAudioFile test

--- tests/ReadAudioFile.cpp
+++ tests/ReadAudioFile.cpp
@@ -2,7 +2,11 @@
 #include <cassert>
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-    
+    auto audioFile = shone::core::AudioFile{"Sine_wave_440.wav"};
+    assert(audioFile.sampleRate() == 132000);
+    assert(audioFile.numChannels() == 1);
+    assert(audioFile.samples().size() == 660000);
+    return EXIT_SUCCESS;
 }
