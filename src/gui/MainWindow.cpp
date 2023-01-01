ee88d46bc6356cceaba8e958b0873fffec04c7db
sakertooth <sakertooth@gmail.com>
2023-01-01 03:06:56 -0500

    Set MainGui as central widget

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -2,12 +2,16 @@
 #include <QMenuBar>
 
 #include "shone/gui/MainWindow.hpp"
+#include "shone/gui/MainGui.hpp"
 
-namespace shone::gui 
+namespace shone::gui
 {
     MainWindow::MainWindow() : QMainWindow()
     {
         setWindowTitle("Shone");
         resize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        
+        auto mainGui = new MainGui{this};
+        setCentralWidget(mainGui);
     }
 }
